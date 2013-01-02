4f095b71cb6c46bf88396359a95ae9894c67dec4
sonnylazuardi <sonnylazuardi@gmail.com>
2013-01-02 19:22:26 +0700

    tampilan1

--- application/views/arsip.php
+++ application/views/arsip.php
@@ -47,5 +47,17 @@
 	  </tr>
 
 	</table>
+
+	<div class="pagination">
+    <ul>
+      <li><a href="#">«</a></li>
+      <li><a href="#">1</a></li>
+      <li><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">4</a></li>
+      <li><a href="#">5</a></li>
+      <li><a href="#">»</a></li>
+    </ul>
+  </div>
 </div>
 </div>
