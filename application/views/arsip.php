b76fa7d6aac19c0263e8eab9116762c770585f17
sonnylazuardi <sonnylazuardi@gmail.com>
2013-01-02 17:24:54 +0700

    ganti rak jadi dua tingkat

---
+++ application/views/arsip.php
@@ -0,0 +1,47 @@
+<div style="height:30px"></div>
+<div class="strip">
+<div class="container">
+	<h2>List Arsip </h2>
+			<form action = "#" method="POST" class="form-inline">
+			<label> Sort berdasarkan </label>
+			<select name="sort">
+			<option>Mata kuliah</option>
+			<option>Bidang</option>
+			<option>Tahun</option>
+			</select>
+			<button type="submit" class="btn btn-primary">Sort</button>
+			</form>
+	<table class="table table-striped">
+	  <tr>
+		  <td style="color:blue"> No </td>
+		  <td style="color:blue"> Judul </td>
+		  <td style="color:blue"> Pengarang </td>
+		  <td style="color:blue"> Tahun Terbit </td>
+		  <td style="color:blue"> Mata Kuliah </td>
+		  <td style="color:blue"> Bidang </td>
+		  <td style="color:blue"> Link </td>
+	  </tr>
+	  
+	  <tr>
+		  <td> 1 </td>
+		  <td> Aplikasi Induksi matematik dalam kehidupan sehari-hari </td>
+		  <td> Anonim </td>
+		  <td> 2011 </td>
+		  <td> Struktur Diskrit </td>
+		  <td> Induksi Matematik </td>
+		  <td> <a href = "?">Lihat</a> </td>
+	  </tr>
+	  
+	  <tr>
+		  <td> 2 </td>
+		  <td> Aplikasi algoritma greedy dalam menyusun salad pizza hut </td>
+		  <td> Alexander Sadiku </td>
+		  <td> 2012 </td>
+		  <td> Strategi Algoritma </td>
+		  <td> Algoritma </td>
+		  <td> <a href = "?">Lihat</a> </td>
+	  </tr>
+
+	</table>
+</div>
+</div>
