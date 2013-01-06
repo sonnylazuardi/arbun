47d033d1d4e7c0e262880bae1b8c502ef9f9559b
sonnylazuardi <sonnylazuardi@gmail.com>
2013-01-06 14:09:21 +0700

    +fiks view

--- application/views/arsip/view.php
+++ application/views/arsip/view.php
@@ -12,6 +12,23 @@
   </div>
 </div>
 
+  <style>
+  .pdfpage { position:relative; top: 0; left: 0; border: solid 1px black; margin: 10px; }
+  .pdfpage > canvas { position: absolute; top: 0; left: 0; }
+  .pdfpage > div { position: absolute; top: 0; left: 0; }
+  .inputControl { background: transparent; border: 0px none; position: absolute; margin: auto; }
+  .inputControl[type='checkbox'] { margin: 0px; }
+  .inputHint { opacity: 0.2; background: #ccc; position: absolute; }
+  </style>
+
+<div class="strip strip-page">
+	<div class="container">
+    <div class="loader"></div>  
+		<div id="viewer">
+    </div>
+	</div>
+</div>
+
   <script type="text/javascript" src="<?php echo base_url() ?>public/js/pdfjs/core.js"></script>
   <script type="text/javascript" src="<?php echo base_url() ?>public/js/pdfjs/util.js"></script>
   <script type="text/javascript" src="<?php echo base_url() ?>public/js/pdfjs/api.js"></script>
@@ -42,20 +59,3 @@
     var pdfWithFormsPath = '<?php echo base_url() ?>public/pdf/tes.pdf';
   </script>
   <script type="text/javascript" src="<?php echo base_url() ?>public/js/tes.js"></script>
-
-  <style>
-  .pdfpage { position:relative; top: 0; left: 0; border: solid 1px black; margin: 10px; }
-  .pdfpage > canvas { position: absolute; top: 0; left: 0; }
-  .pdfpage > div { position: absolute; top: 0; left: 0; }
-  .inputControl { background: transparent; border: 0px none; position: absolute; margin: auto; }
-  .inputControl[type='checkbox'] { margin: 0px; }
-  .inputHint { opacity: 0.2; background: #ccc; position: absolute; }
-  </style>
-
-<div class="strip strip-page">
-	<div class="container">
-    <div class="loader"></div>  
-		<div id="viewer">
-    </div>
-	</div>
-</div>
--- application/views/user/register.php
+++ application/views/user/register.php
@@ -1,66 +1,62 @@
 <div class="page">
 	<div class="row">
-		<div class="span8 offset5">
-				<div style="font-size:20px;color: rgb(255, 255, 255);background-color:white;border-radius:8px;box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.6);margin-bottom:20px;padding-bottom:20px">
-					<p style="font-size:25px; padding-left:10px;padding-top:20px; color: rgb(0, 130, 196);">Registrasi</p>
+		<div class="span8 offset5 strip box">
+					<h3>Registrasi</h3>
 					<legend></legend>
-					<div class="row" style="padding-left:30px">
-						<div class="span4" style="float:left; margin-left:20px">
-							<p style="font-size:14px;padding-left:10px;color:black">Nama :</p>
-							<input class="span3" style="margin-left:10px" type="text" >
-							<p style="font-size:14px;padding-left:10px;color:black">NIM :</p>
-							<input class="span3" style="margin-left:10px" type="text" >
-							<p style="font-size:14px;padding-left:10px;color:black">Email :</p>
-							<input class="span3" style="margin-left:10px" type="email" required>
-							<p style="font-size:14px;padding-left:10px;color:black">Tanggal Lahir :</p>
-							<select class="span1" style="margin-left:10px">
+					<div class="row" style="padding:0 20px 20px 20px;">
+						<div class="span4">
+							<label>Nama :</label>
+							<input class="span3" type="text" >
+							<label>NIM :</label>
+							<input class="span3" type="text" >
+							<label>Email :</label>
+							<input class="span3" type="email" required>
+							<label>Tanggal Lahir :</label>
+							<select class="span1">
 								<option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option>
 								<option>11</option><option>12</option><option>13</option><option>14</option><option>15</option><option>16</option><option>17</option><option>18</option><option>19</option><option>20</option>
 								<option>21</option><option>22</option><option>23</option><option>24</option><option>25</option><option>26</option><option>27</option><option>28</option><option>29</option><option>30</option>
 								<option>31</option>
 							</select>
-							<select style="margin-left:1px; width:100px">
+							<select style="width:100px">
 								<option>Januari</option><option>Februari</option><option>Maret</option><option>April</option><option>Mei</option><option>Juni</option>			
 								<option>Juli</option><option>Agustus</option><option>September</option><option>Oktober</option><option>November</option><option>Desember</option>
 							</select>
-							<input class="span1" style="margin-left:0px" type="text" >
+							<input class="span1" type="text" >
 						</div>
 						<div class="span3" style="float:left; margin-left:40px">
-							<p style="font-size:14px;padding-left:10px;color:black">Status :</p>
-							<select class="span3" style="margin-left:10px">
+							<label>Status :</label>
+							<select class="span3">
 								<option>Dosen</option>
 								<option>Mahasiswa</option>
 								<option>Staf</option>
 								<option>Lain-Lain</option>
 							</select>
-							<p style="font-size:14px;padding-left:10px;color:black">Fakultas/Sekolah :</p>
-							<select class="span3" style="margin-left:10px">
+							<label>Fakultas/Sekolah :</label>
+							<select class="span3">
 								<option>STEI</option>
 								<option>FTMD</option>
 							</select>
-							<p style="font-size:14px;padding-left:10px;color:black">Jurusan :</p>
-							<select class="span3" style="margin-left:10px">
+							<label>Jurusan :</label>
+							<select class="span3">
 								<option>Teknik Informatika</option>
 								<option>Sistem dan Teknologi Informasi</option>
 							</select>
-							<p style="font-size:14px;padding-left:10px;color:black">Jenis Kelamin :</p>
-							<select class="span3" style="margin-left:10px">
+							<label>Jenis Kelamin :</label>
+							<select class="span3">
 								<option>Laki-Laki</option>
 								<option>Perempuan</option>
 							</select>
 						</div>
-					</div>
-					<div class="row" style="padding-left:30px">
-						<div class="span4" style="float:left; margin-left:20px">
-							<p style="font-size:14px;padding-left:10px;color:black">Password :</p>
-							<input class="span3" style="margin-left:10px" type="text" />
-							<p style="font-size:14px;padding-left:10px;color:black">Ulangi Password :</p>
-							<input class="span3" style="margin-left:10px" type="password" />
-							<p style="font-size:14px;padding-left:10px;color:black">Captcha :</p>
-							<button type="submit" style="margin-left:10px" class="btn btn-success">Registrasi</button>
+						<div class="span4">
+							<label>Password :</label>
+							<input class="span3" type="text" />
+							<label>Ulangi Password :</label>
+							<input class="span3" type="password" />
+							<label>Captcha :</label>
+							<button type="submit" class="btn btn-success">Registrasi</button>
 						</div>
 					</div>
-				</div>
 		</div>	
 		
 	</div>
