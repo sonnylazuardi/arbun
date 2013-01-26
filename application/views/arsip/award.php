59cce65d9857c718872a937ce0aa9daef6969c88
sonnylazuardi <sonnylazuardi@gmail.com>
2013-01-26 07:49:47 +0700

    fixed

--- application/views/arsip/award.php
+++ application/views/arsip/award.php
@@ -2,19 +2,22 @@
 	<div class="row">
 		<div class="span6 offset3 strip box">
 			<div class="side">
-				<h3>Tambah Penghargaan</h3>
+				<h4><i class="icon-pencil"></i> Tambah Penghargaan</h4>
 				<legend></legend>
 				<?php $this->load->view('theme/validation'); ?>
 				<?php echo form_open() ?>
 				<div>
-					Judul Buku :<h4><?php echo $buku->judul;?></h4>
+					<img src="<?php echo $buku->get_cover() ?>" width="65px" class="sauto" alt="" />
+					<strong><?php echo $buku->judul;?></strong>
 				</div>
-				<div>Penulis : <?php echo $buku->akun->get()->nama;?></div>
-				<label>Nama Penghargaan :</label>
-				<?php echo form_input('data[nama]', $model->nama, 'style="width:95%" placeholder="Makalah TA terbaik tahun 2013"'); ?>
-				<input type="submit" name="submit" value="Tambah" class="btn btn-primary btn-large">
-				<?php echo anchor('arsip/view/'.$buku->id, 'Batal', 'class="btn"') ?>
-				<?php echo form_close() ?>
+				<div><?php echo $buku->akun->get()->nama;?></div>
+				<div>
+					<label>Nama Penghargaan :</label>
+					<?php echo form_input('data[nama]', $model->nama, 'style="width:95%" placeholder="Makalah TA terbaik tahun 2013"'); ?>
+					<input type="submit" name="submit" value="Tambah" class="btn btn-primary btn-large">
+					<?php echo anchor('arsip/view/'.$buku->id, 'Batal', 'class="btn"') ?>
+					<?php echo form_close() ?>
+				</div>
 			</div>
 		</div>
 	</div>
--- application/views/arsip/report.php
+++ application/views/arsip/report.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="span6 offset3 strip box">
 			<div class="side">
-				<h3>Lapor Makalah</h3>
+				<h4><i class="icon-bullhorn"></i> Lapor Makalah</h4>
 				<legend></legend>
 				<?php $this->load->view('theme/validation'); ?>
 				<p>
@@ -11,14 +11,17 @@
 				
 				<?php echo form_open() ?>
 				<div>
-					Judul Buku :<h4><?php echo $buku->judul;?></h4>
+					<img src="<?php echo $buku->get_cover() ?>" width="65px" class="sauto" alt="" />
+					<strong><?php echo $buku->judul;?></strong>
 				</div>
-				<div>Penulis : <?php echo $buku->akun->get()->nama;?></div>
-				<label>Alasan Laporan :</label>
-				<?php echo form_textarea('lapor', $model->isi, 'style="width:95%" placeholder="Makalah ini mengandung SARA pada paragraf kedua kalimat pertama"'); ?>
-				<input type="submit" name="submit" value="Lapor" class="btn btn-primary btn-large">
-				<?php echo anchor('arsip/view/'.$buku->id, 'Batal', 'class="btn"') ?>
-				<?php echo form_close() ?>
+				<div><?php echo $buku->akun->get()->nama;?></div>
+				<div>
+					<label>Alasan Laporan :</label>
+					<?php echo form_textarea('lapor', $model->isi, 'style="width:95%; height:100px" placeholder="Makalah ini mengandung Plagiarisme pada paragraf kedua kalimat pertama"'); ?>
+					<input type="submit" name="submit" value="Lapor" class="btn btn-primary btn-large">
+					<?php echo anchor('arsip/view/'.$buku->id, 'Batal', 'class="btn"') ?>
+					<?php echo form_close() ?>
+				</div>
 			</div>
 		</div>
 	</div>
