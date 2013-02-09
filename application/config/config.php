4aaa3b1e7b40a871c2ed1aeef458645f22426f89
sonnylazuardi <sonnylazuardi@gmail.com>
2013-02-09 08:43:17 +0700

    hhe

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/arbun';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
